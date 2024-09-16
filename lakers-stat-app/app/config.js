5b456c20e20d508c05784508457ef0622314b06c
JASPER <jasjasper101@gmail.com>
2024-09-16 13:03:31 -0700

    Implement .env variables

---
+++ lakers-stat-app/app/config.js
@@ -0,0 +1,3 @@
+export const pointsAPI = process.env.NEXT_PUBLIC_POINTS_API || '';
+export const reboundsAPI = process.env.NEXT_PUBLIC_REBOUNDS_API || '';
+export const threePointersAPI = process.env.NEXT_PUBLIC_THREE_POINTERS_API || '';
