4eca565706e7b6db4362ce4ed4bbe0bfb7c3d179
JASPER <jasjasper101@gmail.com>
2024-11-16 08:05:19 -0800

    Implement APIs

--- lakers-stat-app/app/config.js
+++ lakers-stat-app/app/config.js
@@ -1,3 +1,3 @@
-export const pointsAPI = process.env.NEXT_PUBLIC_POINTS_API || '';
-export const reboundsAPI = process.env.NEXT_PUBLIC_REBOUNDS_API || '';
-export const threePointersAPI = process.env.NEXT_PUBLIC_THREE_POINTERS_API || '';
+export const pointsAPI = "https://data.nba.com/data/5s/v2015/json/mobile_teams/nba/2020/teams/statistics/lakers/leaders_detail_points_04.json"
+export const reboundsAPI = "https://data.nba.com/data/5s/v2015/json/mobile_teams/nba/2020/teams/statistics/lakers/leaders_detail_rebounds_04.json"
+export const threePointersAPI = "https://data.nba.com/data/5s/v2015/json/mobile_teams/nba/2020/teams/statistics/lakers/leaders_detail_threepointers_04.json"
